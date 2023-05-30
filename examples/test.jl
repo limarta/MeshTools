c65d77daae8f49a4f51c4eacda73c6de77d399d1
Ian Limarta <ian.limarta@gmail.com>
2023-05-30 15:46:59 -0400

    .

--- examples/test.jl
+++ examples/test.jl
@@ -4,14 +4,13 @@ using LinearAlgebra
 using SparseArrays
 using BenchmarkTools
 using LinearMaps
-import LinearAlgebra: ldiv!
-using SparseArrays: UMFPACK, LibSuiteSparse
 
 # TODO: Check umfpack
 V,F = MeshTools.readoff("examples/michael1.off")
 mesh = MeshTools.Mesh(V,F)
 L = cot_laplacian(mesh)
 A = MeshTools.vertex_area(V,F)
+λ, ϕ = eigs(L, nev=128, sigma=0)
 
 
 # Define a matrix whose eigenvalues you want
@@ -23,14 +22,16 @@ end
 
 # Target the largest eigenvalues of the inverted problem
 function eig(L)
-    decomp, = partialschur(construct_linear_map(L), nev=128, tol=1e-5, restarts=100, which=LM())
+    decomp, = partialschur(construct_linear_map(L), nev=128, tol=1e-10, restarts=100, which=LM())
     λs_inv, X = partialeigen(decomp)
 end
-eig(L)
-@btime eig($L)
+λs_inv, X =  eig(L)
 
 # Eigenvalues have to be inverted to find the smallest eigenvalues of the non-inverted problem.
-# λs = 1 ./ λs_inv
+λs = 1 ./ λs_inv
+println(λs)
+println()
+println(λ)
  
 # Show that Ax = xλ
 # @show norm(A * X - X * Diagonal(λs)) # 7.38473677258669e-6
